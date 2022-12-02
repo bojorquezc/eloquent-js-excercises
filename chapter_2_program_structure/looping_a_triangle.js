2b189fea52a5e0cc031e89a3b3237e6ebd0fc193
Conrado <42232126+bojorquezc@users.noreply.github.com>
2022-12-01 21:15:16 -0800

    Add chapter 2 looping triangle excercise

---
+++ chapter_2_program_structure/looping_a_triangle.js
@@ -0,0 +1,23 @@
+/*looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.*/
+
+let symbol = "#";
+for (counter = 0; counter <= 6; counter +=1) {
+    console.log(symbol);
+    symbol += "#";
+}
